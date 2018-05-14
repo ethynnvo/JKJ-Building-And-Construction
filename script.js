93efa5f7a41711b08a8fb63fab31ac602d8a3983
Ethynn Van Os <ethynnvanos@Ethynns-MacBook-Pro.local>
2018-05-14 22:52:38 +0200

    fleshed out more of the site

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ const toggleBurger = () => {
   burger.classList.toggle("is-active");
   menu.classList.toggle("is-active");
 }; 
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction () {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		document.getElementById("myBtn").style.display = "block";
+	} else {
+		document.getElementById("myBtn").style.display = "none";
+	}
+};
+
+function topFunction() {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+};
